b5d53f5d71b301724122aabff10d112c6fac8315
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-07 21:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	let s='';
+	if(s.length==0){
+		return '';
+	}
+	
+	for(let i=0;i<s.length;i++){
+		if(s.charAt[i]==' ' || i==s.length-1){
+			return s;
+		}
+			s=s+s.charAt[i];
+		}
+	return s;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+/*const s = prompt("Enter String:");
+alert(firstWord(s));*/
